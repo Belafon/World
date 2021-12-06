aea91b0bc2094edf2900934703e92a71c49f2dfc
pvpsurvivaltest <tichavskyo@seznam.cz>
2021-12-06 17:20:12 +0100

    remove World

--- World/src/Game/Maps/Resources/Resource.java
+++
@@ -1,8 +0,0 @@
-package Game.Maps.Resources;
-
-public class Resource {
-    public final TypeOfResourceWithPressence typeOfResourceWithPressence;
-    public Resource(TypeOfResourceWithPressence typeOfResource) {
-        this.typeOfResourceWithPressence = typeOfResource;
-    }
-}
