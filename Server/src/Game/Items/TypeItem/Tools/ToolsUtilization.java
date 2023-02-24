ac56ccd07cf2d9259221af64f2d18e258f045f94
Ondřej <tichavskyo@seznam.cz>
2023-02-24 14:04:45 +0100

    ToolsUtilization class added

---
+++ Server/src/Game/Items/TypeItem/Tools/ToolsUtilization.java
@@ -0,0 +1,7 @@
+package Game.Items.TypeItem.Tools;
+
+import Game.Creatures.Behaviour.Behaviours.BehavioursPossibleRequirement;
+
+public enum ToolsUtilization implements BehavioursPossibleRequirement {
+    
+}
