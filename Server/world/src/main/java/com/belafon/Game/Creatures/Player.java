47c392784a6f630b50f6a57329edfe6ba6d3cdaa
Ondřej <tichavskyo@seznam.cz>
2023-04-12 11:38:37 +0200

    sends maps id instead of hash code

--- Server/world/src/main/java/com/belafon/Game/Creatures/Player.java
+++ Server/world/src/main/java/com/belafon/Game/Creatures/Player.java
@@ -15,18 +15,17 @@ import com.belafon.Game.Items.ListOfAllItemTypes.NamesOfFoodItemTypes;
 public class Player extends Creature {
     public final Client client;
     public volatile boolean isReady = false; 
-    public Player(Client client, World game, String name, Place position, int id, String appearence){
-        super(game, name, position, id, appearence, client.writer.sender, 100); // TODO weight
+    public Player(Client client, World game, String name, Place position, String appearence){
+        super(game, name, position, appearence, client.writer.sender, 100); // TODO weight
         this.client = client;
         //super(position, client.name, game, id);
     	setAbilityCondition(0, 0, 300, 600, 100, 0, 100);
-        setInventory();
         sendInfoAboutMaps();
     }
 
     private void sendInfoAboutMaps() {
         for (Map map : game.maps.maps) {
-            writer.surrounding.setNewMap(map.hashCode(), map.sizeX, map.sizeY);
+            writer.surrounding.setNewMap(map, map.sizeX, map.sizeY);
         }
     }
     @Override
@@ -34,8 +33,8 @@ public class Player extends Creature {
 		inventory = new Inventory(new PlayersGear(), this);
 	}
     public void gameStart() {
-        inventory.addItem(new Food(game, 0, 100, new SpecialFoodsProperties[] {},
-                ListOfAllItemTypes.foodTypes.get(NamesOfFoodItemTypes.apple)));
+        inventory.addItem(new Food(game, 5, 100, new SpecialFoodsProperties[] {},
+                ListOfAllItemTypes.foodTypes.get(NamesOfFoodItemTypes.apple), position));
         
         client.writer.surrounding.setInfoAboutSurrounding(position);
         //setBehaviour(new Move(game, this, game.maps.maps[0].places[1][1]));
