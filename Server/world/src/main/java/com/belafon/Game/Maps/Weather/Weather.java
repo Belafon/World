56e7dee694c8a9eca9304bb2309335351ae5ca1b
Ondřej <tichavskyo@seznam.cz>
2023-04-12 11:37:49 +0200

    send info about wather to creature

--- Server/world/src/main/java/com/belafon/Game/Maps/Weather/Weather.java
+++ Server/world/src/main/java/com/belafon/Game/Maps/Weather/Weather.java
@@ -4,33 +4,49 @@ import com.belafon.Game.Creatures.Creature;
 import com.belafon.Game.Maps.Place.Place;
 
 public class Weather {
-    public enum TypeWeather{
+    public enum TypeWeather {
         nothing, rain, heavy_rain, storm, thunderstorm
     }
-    public volatile int durationOfRain = 0; 
+
+    public volatile int durationOfRain = 0;
     private volatile int clouds;
     private int weather = 0;
     public volatile int visibility = 0; // can be changed by fog for example
+
     public Weather(int durationOfRain, int clouds, int weather) {
         this.durationOfRain = durationOfRain;
         this.clouds = clouds;
         this.weather = weather;
     }
+
     public int getWeather() {
         return weather;
     }
+
     public void setWeather(int weather, Place place) {
         this.weather = weather;
-        for(Creature creature : place.creatures)creature.writer.surrounding.setWeather(this);
+        place.getCreatures((creatures) -> {
+            for (Creature creature : creatures)
+                creature.writer.surrounding.setWeather(this);
+            return null;
+        });
     }
+
     public int getClouds() {
         return clouds;
     }
+
     public void setClouds(int clouds, Place place) {
         this.clouds = clouds;
-        for(Creature creature : place.creatures)creature.writer.surrounding.setClouds(creature.getLocation());
+        place.getCreatures((creatures) -> {
+            for (Creature creature : creatures)
+                creature.writer.surrounding.setClouds(place);
+            return null;
+        });
+        
 
     }
+
     public int getVisibility() {
         return visibility;
     }
