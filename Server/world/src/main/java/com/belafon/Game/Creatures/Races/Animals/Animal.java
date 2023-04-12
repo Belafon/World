ebc6367e90164e715ee37052f59b5b62cdb1369f
Ondřej <tichavskyo@seznam.cz>
2023-04-12 11:33:17 +0200

    Animals added

---
+++ Server/world/src/main/java/com/belafon/Game/Creatures/Races/Animals/Animal.java
@@ -0,0 +1,37 @@
+package com.belafon.Game.Creatures.Races.Animals;
+
+import com.belafon.Game.World;
+import com.belafon.Game.Creatures.Creature;
+import com.belafon.Game.Creatures.Inventory.Gear;
+import com.belafon.Game.Creatures.Inventory.Inventory;
+import com.belafon.Game.Creatures.Races.Animals.AnimalRaces.AnimalRace;
+import com.belafon.Game.Items.Types.Clothes;
+import com.belafon.Game.Maps.Place.Place;
+
+public class Animal extends Creature {
+    public final AnimalRace race;
+
+    public Animal(World game, String name, Place position, String appearence,
+            int weight, AnimalRace race) {
+        super(game, name, position, appearence, race.sendMessage, weight);
+        this.race = race;
+    }
+
+    @Override
+    protected void setInventory() {
+        inventory = new Inventory(new Gear() {
+
+            @Override
+            public boolean putOn(Clothes clothes) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'putOn'");
+            }
+
+            @Override
+            public boolean putOff(Clothes clothes) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'putOff'");
+            }
+        }, this);
+    }
+}
---
+++ Server/world/src/main/java/com/belafon/Game/Creatures/Races/Animals/TestingMessages.java
@@ -0,0 +1,33 @@
+package com.belafon.Game.Creatures.Races.Animals;
+
+import com.belafon.Server.Messages.BehavioursMessages;
+import com.belafon.Server.Messages.ConditionCreatureMessages;
+import com.belafon.Server.Messages.CreatureVisibleMessages;
+import com.belafon.Server.Messages.InventoryMessages;
+import com.belafon.Server.Messages.ItemVisibleMessages;
+import com.belafon.Server.Messages.ResourceVisibleMessages;
+import com.belafon.Server.Messages.ServerMessages;
+import com.belafon.Server.Messages.SurroundingMessages;
+import com.belafon.Server.SendMessage.MessageSender;
+
+public class TestingMessages
+    implements BehavioursMessages, ConditionCreatureMessages,
+        InventoryMessages, ItemVisibleMessages,
+        ResourceVisibleMessages, SurroundingMessages,
+        ServerMessages, CreatureVisibleMessages {
+    public static MessageSender createMessageSender() {
+        final TestingMessages messages = new TestingMessages();
+
+        MessageSender sender = MessageSender.getBuilder()
+            .setBehavioursMessages(messages)
+            .setCondition(messages)
+            .setCreatureVisible(messages)
+            .setItemVIsible(messages)
+            .setResourceVisible(messages)
+            .setServer(messages)
+            .setSurrounding(messages)
+            .setInventory(messages)
+            .build();
+        return sender; 
+    }
+}
