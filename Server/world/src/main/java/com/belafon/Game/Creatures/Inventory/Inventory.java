4486e836fb454b5cf44b3a7ee555f170ef4f157f
Ondřej <tichavskyo@seznam.cz>
2023-04-12 11:34:05 +0200

    back changed to back space place

--- Server/world/src/main/java/com/belafon/Game/Creatures/Inventory/Inventory.java
+++ Server/world/src/main/java/com/belafon/Game/Creatures/Inventory/Inventory.java
@@ -2,8 +2,7 @@ package com.belafon.Game.Creatures.Inventory;
 
 import com.belafon.Console.ConsolePrint;
 import com.belafon.Game.Items.Types.*;
-import com.belafon.Game.Maps.Place.ListOfAllTypesOfPlaces;
-import com.belafon.Game.Maps.Place.UnboundedPlace;
+import com.belafon.Game.Items.Types.SpaceItem.BuilderSpaceItem;
 import com.belafon.Game.Creatures.Creature;
 import com.belafon.Game.Items.Item;
 import com.belafon.Game.Items.ListOfAllItemTypes;
@@ -12,23 +11,24 @@ import com.belafon.Game.Items.TypeItem.Tools.ToolsUtilization;
 
 public class Inventory {
     public volatile int totalWeight;
-	public volatile int currentWeight;
+    public volatile int currentWeight;
 
     private SpaceItem backSpace;
 
-	public final Gear gear;
+    public final Gear gear;
     public final Creature creature;
 
     public Inventory(Gear gear, Creature creature) {
         this.gear = gear;
         this.creature = creature;
-        backSpace = new SpaceItem(creature.game,
-            ListOfAllItemTypes.spaceItems.get(ListOfAllItemTypes.NamesOfSpaceItemTypes.back_space),
-            new UnboundedPlace(ListOfAllTypesOfPlaces.typeOfPlaces.get(ListOfAllTypesOfPlaces.NamesOfTypesOfPlaces.back_space), creature.game));
+        backSpace =  new BuilderSpaceItem(creature.game,
+                ListOfAllItemTypes.spaceItems.get(ListOfAllItemTypes.NamesOfSpaceItemTypes.back_space),
+                creature.getLocation())
+            .build(creature.game);
     }
 
     public volatile Tool leftHand;
-	public volatile Tool workHand;
+    public volatile Tool workHand;
 
     // item has moved
     public synchronized void addItem(Item item) {
@@ -36,14 +36,15 @@ public class Inventory {
             // TODO call unsupported operation
             ConsolePrint.error("Try to add item to inventory without bag");
         }
-        backSpace.getSpace().items.add(item);
-        // creature.behaviourCondition.addBehavioursPossibleRequirement(item.type, item); -> wrong 
+        backSpace.getSpace().addItem(item);
+        // creature.behaviourCondition.addBehavioursPossibleRequirement(item.type,
+        // item); -> wrong
         // because that item could be reachable before that
 
         item.location = backSpace.getLocation();
-        
+
         if (item instanceof Tool tool) {
-            if(tool.type instanceof ToolTypeItem toolType){
+            if (tool.type instanceof ToolTypeItem toolType) {
                 for (ToolsUtilization toolsUtilization : toolType.toolsUtilizations) {
                     creature.behaviourCondition.addBehavioursPossibleRequirement(toolsUtilization, item);
                 }
@@ -53,9 +54,9 @@ public class Inventory {
         creature.writer.inventory.setAddItem(item);
     }
 
-	public synchronized void removeItem(Item item) {
+    public synchronized void removeItem(Item item) {
 
-		// lets send info about it
-		creature.writer.inventory.setRemoveItem(item);
-	}
+        // lets send info about it
+        creature.writer.inventory.setRemoveItem(item);
+    }
 }
