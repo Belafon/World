59db10c0ba53c2bf0ceca5d1b28b25d1caec7951
Ondřej <tichavskyo@seznam.cz>
2023-04-18 12:17:28 +0200

    Behaviour deprecated constructor removed

--- Server/world/src/main/java/com/belafon/Game/Creatures/Behaviour/Behaviour.java
+++ Server/world/src/main/java/com/belafon/Game/Creatures/Behaviour/Behaviour.java
@@ -7,7 +7,6 @@ import com.belafon.Game.Calendar.Events.EventBehaviour;
 import com.belafon.Game.Creatures.Creature;
 import com.belafon.Game.Creatures.Behaviour.BehaviourType.IngredientsCounts;
 import com.belafon.Game.Creatures.Behaviour.Behaviours.BehavioursPossibleRequirement;
-import com.belafon.Game.Creatures.Behaviour.Behaviours.ConsumableBehavioursPossibleRequirement;
 
 public abstract class Behaviour {
     protected World game;
@@ -15,23 +14,12 @@ public abstract class Behaviour {
     protected final int bodyStrain;
     protected final Creature creature;
     protected EventBehaviour event;
-    public BehavioursPossibleRequirement[] requirements;
 
     public Behaviour(World game, int duration, int bodyStrain, Creature creature) {
         this.game = game;
         this.duration = duration;
         this.bodyStrain = bodyStrain;
         this.creature = creature;
-        requirements = new BehavioursPossibleRequirement[0];
-    }
-
-    public Behaviour(World game, int duration, int bodyStrain, Creature creature,
-            ConsumableBehavioursPossibleRequirement[] consumableProperties, BehavioursPossibleRequirement[] nonConsumableProperties) {
-        this.game = game;
-        this.duration = duration;
-        this.bodyStrain = bodyStrain;
-        this.creature = creature;
-        this.requirements = nonConsumableProperties;
     }
 
     public abstract void execute();
