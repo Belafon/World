6c62a4a97ba3010b2fa96a60d5291e9f15319519
Ondřej <tichavskyo@seznam.cz>
2023-06-22 12:56:16 +0200

    stats and fragments added

--- MobileClient/World/app/src/main/java/com/example/world/Client/Client.java
+++ MobileClient/World/app/src/main/java/com/example/world/Client/Client.java
@@ -17,10 +17,10 @@ import java.net.UnknownHostException;
 public class Client {
     private static final String TAG = "Client";
     public static volatile Socket clientSocket;
-    public static volatile String ip = "192.168.0.106";
+    public static final String ip = "192.168.0.106";
     public static volatile int port = 25555;
     public static volatile boolean flow = false; // controls the network connection
-    public static volatile String name = "";
+    public static String name = "";
     public static final DataLibrary clientsData = new DataLibrary("clientsData");
     public static volatile boolean disconnected = true;
     public static volatile int condition; // 0 -> connected, 1 -> reconnected, 2 -> first start
@@ -28,6 +28,8 @@ public class Client {
     public static final int playing = 11;
     public static final int first_start = 12;
     public static volatile int actualGameId = -1;
+    public static volatile Stats stats;
+    public static volatile Fragments fragments;
 
     public Client(){
         condition = clientsData.LoadDataInteager(AbstractActivity.actualActivity, "clientsCondition");
@@ -148,108 +150,14 @@ public class Client {
     //private static Timer timer;
     public synchronized static void decomposeTheString(String value) {
         Log.d(TAG, "decomposeTheString: TRY HELLO WORLD");
-    /*    // TODO Auto-generated method stub
-        final String[] message = value.split(" ");
-        String typeAction;
-        typeAction = message[0];
-        switch(typeAction) {
-            case "get":// getter
-                get(message);
-                break;
-            case "start_the_game":
-                MainActivity.actual_activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((MenuActivity)MainActivity.actual_activity).goToLastActivity = true;
-                        GameActivity.disconnected = false;
-                        Intent menuIntent = new Intent(MainActivity.actual_activity, GameActivity.class);
-                        MainActivity.actual_activity.startActivity(menuIntent);
-                    }
-                });
-                break;
-            case "startGameTimer": // startGameTimer durationOfRound rounds
-                timer = new Timer(Integer.parseInt(message[1]), Integer.parseInt(message[2]));
-                break;
-            case "new_round":
-                MainActivity.actual_activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (MainActivity.actual_activity instanceof GameActivity)
-                            ((TextView)MainActivity.actual_activity.findViewById(R.id.current_round)).setText("Round: " + message[1]);
-                    }
-                });
-                timer.makeNewRound = true;
-                break;
-            case "size_of_map":
-                GameActivity.sizeOfMap = Integer.parseInt(message[1]);
-                GameActivity.map = new Place[GameActivity.sizeOfMap][GameActivity.sizeOfMap];
-                for (int i = 0; i < GameActivity.sizeOfMap; i++) {
-                    for (int j = 0; j < GameActivity.sizeOfMap; j++) {
-                        GameActivity.map[i][j] = new Place(i, j);
-                    }
-                }
-                break;
-            default:
-                Log.d(TAG, "decomposeTheString: Some wierd default!!!!");
-                Log.d(TAG, "decomposeTheString: " + value);
-                break;
-        }*/
 
     }
-   /* public final static Place[][] pl = GameActivity.map;
 
-    private static void get(final String[] message) {
-        switch (message[1]){
-            case "number_of_players_to_wait":
-                DecomposeMessage.number_of_players_to_wait(message);
-                break;
-            case "set_id":
-                DecomposeMessage.set_id(message);
-                break;
-            case "set_wood":
-                DecomposeMessage.set_wood(message);
-                break;
-            case "set_stone":
-                DecomposeMessage.set_stone(message);
-                break;
-            case "set_food":
-                DecomposeMessage.set_food(message);
-                break;
-            case "set_gold":
-                DecomposeMessage.set_gold(message);
-                break;
-            case "set_force":
-                DecomposeMessage.set_force(message);
-                break;
-            case "not_enough_resources":
-                MainActivity.actual_activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.actual_activity, "Not enough resources", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                break;
-            case "addUnit":
-                DecomposeMessage.addUnit(message);
-                break;
-            case "removeUnit":
-                DecomposeMessage.remove_unit(message);
-                break;
-            case "addVillage":
-                DecomposeMessage.addVillage(message);
-                break;
-            case "removeVillage":
-                DecomposeMessage.remove_village(message);
-                break;
-            case "addVision": // get addVision  3 3 map.Meadow 3 4 map.Meadow 3 5 map.Forest
-                DecomposeMessage.addVision(message);
-                break;
-            case "removeVision"://get removeVision
-                DecomposeMessage.removeVision(message);
-                break;
-            case "setResoult":
-                DecomposeMessage.setResoult(message);
-                break;
-        }
-    }*/
+    public void setStats(Stats stats) {
+        this.stats = stats;
+    }
+
+    public void setFragments(Fragments fragment) {
+        this.fragments = fragments;
+    }
 }
