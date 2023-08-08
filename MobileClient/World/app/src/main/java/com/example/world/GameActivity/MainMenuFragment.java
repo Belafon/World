528062dea75341796df5c2f5e1f84b36c66fa0f7
Ondřej <tichavskyo@seznam.cz>
2023-08-08 12:24:30 +0200

    main menu updated, vertical list of image buttons

--- MobileClient/World/app/src/main/java/com/example/world/GameActivity/MainMenuFragment.java
+++ MobileClient/World/app/src/main/java/com/example/world/GameActivity/MainMenuFragment.java
@@ -25,40 +25,45 @@ public class MainMenuFragment extends Fragment {
         ImageButton characterButton = view.findViewById(R.id.character);
         characterButton.setOnClickListener(v -> onCharacter());
 
+        ImageButton visiblesButton = view.findViewById(R.id.visibles);
+        visiblesButton.setOnClickListener(v -> onVisibles());
 
         /*
-        ImageButton inventoryButton = view.findViewById(R.id.inventory);
-        inventoryButton.setOnClickListener(v -> onInventory());
-
+         * ImageButton inventoryButton = view.findViewById(R.id.inventory);
+         * inventoryButton.setOnClickListener(v -> onInventory());
+         */ 
         ImageButton behaviourButton = view.findViewById(R.id.behaviour);
         behaviourButton.setOnClickListener(v -> onBehaviour());
-
+          
         ImageButton mapButton = view.findViewById(R.id.map);
         mapButton.setOnClickListener(v -> onMap());
-*/
+         
         ImageButton viewButton = view.findViewById(R.id.view);
         viewButton.setOnClickListener(v -> onView());
 
         return view;
     }
 
+    private void onVisibles() {
+        gameActivity.openFragment(gameActivity.game.panels.visibles);
+    }
+
     public void onCharacter() {
         gameActivity.openFragment(gameActivity.game.panels.bodyStatistics);
     }
 
-    /*
-     * public void onInventory() {
-     * gameActivity.openFragment(gameActivity.game.panels.inventory);
-     * }
-     * 
-     * public void onBehaviour() {
-     * gameActivity.openFragment(gameActivity.game.panels.behaviours);
-     * }
-     * 
-     * public void onMap() {
-     * gameActivity.openFragment(gameActivity.game.panels.surroundingPlaces);
-     * }
-     */
+/*     public void onInventory() {
+        gameActivity.openFragment(gameActivity.game.panels.inventory);
+    }
+ */
+    public void onBehaviour() {
+        gameActivity.openFragment(gameActivity.game.panels.behaviours);
+    }
+
+    public void onMap() {
+        gameActivity.openFragment(gameActivity.game.panels.surroundingPlaces);
+    }
+
     public void onView() {
         gameActivity.openFragment(gameActivity.game.panels.view);
     }
