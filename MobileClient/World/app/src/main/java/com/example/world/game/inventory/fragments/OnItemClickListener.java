2969217ecad36b7f3e514951100531259ea4f66e
Ondrej Tichavsky <tichavskyo@seznam.cz>
2023-08-18 18:20:43 +0200

    remove all files to rename folders

--- MobileClient/World/app/src/main/java/com/example/world/game/inventory/fragments/OnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.world.game.inventory.fragments;
-
-import com.example.world.game.visibles.items.Item;
-
-public interface OnItemClickListener {
-    void onItemClick(Item item);
-}
--- MobileClient/World/app/src/main/java/com/example/world/logs/Logs.java
+++
@@ -1,9 +0,0 @@
-package com.example.world.logs;
-
-/**
- * Contains boolean values for logs filtering
- * for saying which logs should be printed and which not.
- */
-public class Logs {
-    public static volatile boolean WEATHER_FILTER = true;
-}
