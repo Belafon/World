401937abd0239d12f3aaeb8373d21ebadab2f8cd
Ondřej <tichavskyo@seznam.cz>
2023-08-30 19:09:10 +0200

    stop logs

--- MobileClient/World/app/src/main/java/com/belafon/world/mobileClient/logs/Logs.java
+++ MobileClient/World/app/src/main/java/com/belafon/world/mobileClient/logs/Logs.java
@@ -5,7 +5,7 @@ package com.belafon.world.mobileClient.logs;
  * for saying which logs should be printed and which not.
  */
 public class Logs {
-    public static volatile boolean ALL_SERVER_MESSAGES = true;
+    public static volatile boolean ALL_SERVER_MESSAGES = false;
     // logs color of background color filter
     public static volatile boolean WEATHER_FILTER = false;
 }
